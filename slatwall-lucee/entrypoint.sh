58d0dbb58123ca6b93055ddf2cb9afa5a08b02cd
Greg Moser <greg@gregmoser.com>
2015-09-23 08:15:50 -0700

    Upgraded to 4.1.011, removed echos, added dbhost

--- slatwall-lucee/entrypoint.sh
+++ slatwall-lucee/entrypoint.sh
@@ -13,30 +13,17 @@ LUCEEWEBSALT='12FB74C3-69CD-4348-A839A29127ED8B7C'
 LUCEESERVERPASS=$LUCEEPASS:$LUCEESERVERSALT
 LUCEEWEBPASS=$LUCEEPASS:$LUCEEWEBSALT
 
-echo 'RAW PASS'
-echo $LUCEESERVERPASS
-echo $LUCEEWEBPASS
-
 for i in `seq 1 5`;
 do
 	LUCEESERVERPASS=($(echo -n $LUCEESERVERPASS | sha256sum))
 	LUCEEWEBPASS=($(echo -n $LUCEEWEBPASS | sha256sum))
 done
 
-echo ==== LUCEE SERVER PASSWORD ====
-echo $LUCEESERVERPASS
-echo $LUCEESERVERSALT
-echo ==== LUCEE WEB PASSWORD ====
-echo $LUCEEWEBPASS
-echo $LUCEEWEBSALT
-
 sed -i "s/\${SERVER-HSPW}/$LUCEESERVERPASS/g" /opt/lucee/server/lucee-server/context/lucee-server.xml
 sed -i "s/\${SERVER-HSPW-SALT}/$LUCEESERVERSALT/g" /opt/lucee/server/lucee-server/context/lucee-server.xml
 sed -i "s/\${MYSQL_ROOT_PASSWORD}/${MYSQL_ROOT_PASSWORD}/g" /opt/lucee/server/lucee-server/context/lucee-server.xml
+sed -i "s/\${MYSQL_HOST}/${MYSQL_HOST}/g" /opt/lucee/server/lucee-server/context/lucee-server.xml
 sed -i "s/\${WEB-HSPW}/$LUCEEWEBPASS/g" /opt/lucee/web/lucee-web.xml.cfm
 sed -i "s/\${WEB-HSPW-SALT}/$LUCEEWEBSALT/g" /opt/lucee/web/lucee-web.xml.cfm
-
-cat /opt/lucee/server/lucee-server/context/lucee-server.xml
-cat /opt/lucee/web/lucee-web.xml.cfm
 
 exec "$@"

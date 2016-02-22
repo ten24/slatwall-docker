26b0eac29cb02c86c97ac5b8ed0e94408ff92e62
Greg Moser <greg@gregmoser.com>
2016-02-22 12:27:24 -0800

    More container updates

--- slatwall-qa/entrypoint.sh
+++ slatwall-qa/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# === COPIED FROM SLATWALL-LUCEE DOCKER FILE ===
-
 # Clear out the www folder
 rm -rf /var/www/*
 
@@ -15,41 +13,28 @@ wget -nv https://github.com/ten24/slatwall/archive/${SLATWALL_VERSION_OR_BRANCH}
 	rm -rf /root/slatwall.zip && \
 	rm -rf /root/slatwall-${SLATWALL_VERSION_OR_BRANCH}
 
-# Copy over the configFramework.cfm file
+# Copy over the configFramework.cfm file so that it displays full errors
 cp /root/configFramework.cfm /var/www/custom/config/configFramework.cfm
 
-# === END: COPIED FROM SLATWALL-LUCEE DOCKER FILE ===
-
-# === COPIED FROM SLATWALL-LUCEE IMAGE ===
-LUCEEPASS=${LUCEE_PASSWORD}
-
-#LUCEESERVERSALT=$(uuidgen)
-#LUCEESERVERSALT=${LUCEESERVERSALT^^}
-LUCEESERVERSALT='4FDA588E-318A-445C-898736AA1F229A69'
-
-#LUCEEWEBSALT=$(uuidgen)
-#LUCEEWEBSALT=${LUCEEWEBSALT^^}
-LUCEEWEBSALT='12FB74C3-69CD-4348-A839A29127ED8B7C'
-
-LUCEESERVERPASS=$LUCEEPASS:$LUCEESERVERSALT
-LUCEEWEBPASS=$LUCEEPASS:$LUCEEWEBSALT
-
-for i in `seq 1 5`;
-do
-	LUCEESERVERPASS=($(echo -n $LUCEESERVERPASS | sha256sum))
-	LUCEEWEBPASS=($(echo -n $LUCEEWEBPASS | sha256sum))
-done
-
+##### UPDATE LUCEE CONFIG FILES #####
+# Update Lucee Server Admin Password
 sed -i "s/\${SERVER-HSPW}/$LUCEESERVERPASS/g" /opt/lucee/server/lucee-server/context/lucee-server.xml
 sed -i "s/\${SERVER-HSPW-SALT}/$LUCEESERVERSALT/g" /opt/lucee/server/lucee-server/context/lucee-server.xml
+
+# Update Lucee Datasource Information
 sed -i "s/\${MYSQL_ROOT_PASSWORD}/${MYSQL_ROOT_PASSWORD}/g" /opt/lucee/server/lucee-server/context/lucee-server.xml
 sed -i "s/\${MYSQL_HOST}/${MYSQL_HOST}/g" /opt/lucee/server/lucee-server/context/lucee-server.xml
+sed -i "s/\${MYSQL_DATABASE}/${MYSQL_DATABASE}/g" /opt/lucee/server/lucee-server/context/lucee-server.xml
+
+# Update Lucee Web Admin Password
 sed -i "s/\${WEB-HSPW}/$LUCEEWEBPASS/g" /opt/lucee/web/lucee-web.xml.cfm
 sed -i "s/\${WEB-HSPW-SALT}/$LUCEEWEBSALT/g" /opt/lucee/web/lucee-web.xml.cfm
-# === END: COPIED FROM SLATWALL-LUCEE IMAGE ===
 
+# Setup MySQL Password
 debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD"
+
+# Install MySQL
 apt-get -y install mysql-server
 
 echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" > /mysql_init.sql
--- slatwall-quickstart/entrypoint.sh
+++ slatwall-quickstart/entrypoint.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
-# === COPIED FROM SLATWALL-LUCEE IMAGE ===
-LUCEEPASS=${LUCEE_PASSWORD}
+# Clear out the www folder
+rm -rf /var/www/*
 
-#LUCEESERVERSALT=$(uuidgen)
-#LUCEESERVERSALT=${LUCEESERVERSALT^^}
-LUCEESERVERSALT='4FDA588E-318A-445C-898736AA1F229A69'
+# Clear out the mysql folder
+rm -rf /var/lib/mysql
 
-#LUCEEWEBSALT=$(uuidgen)
-#LUCEEWEBSALT=${LUCEEWEBSALT^^}
-LUCEEWEBSALT='12FB74C3-69CD-4348-A839A29127ED8B7C'
+# Download Slatwall, unzip and remove Downloaded zip
+wget -nv https://github.com/ten24/slatwall/archive/master.zip -O /root/slatwall.zip && \
+	unzip /root/slatwall.zip -d /root && \
+	cp -a /root/slatwall-master/. /var/www && \
+	rm -rf /root/slatwall.zip && \
+	rm -rf /root/slatwall-master
 
-LUCEESERVERPASS=$LUCEEPASS:$LUCEESERVERSALT
-LUCEEWEBPASS=$LUCEEPASS:$LUCEEWEBSALT
-
-for i in `seq 1 5`;
-do
-	LUCEESERVERPASS=($(echo -n $LUCEESERVERPASS | sha256sum))
-	LUCEEWEBPASS=($(echo -n $LUCEEWEBPASS | sha256sum))
-done
-
+##### UPDATE LUCEE CONFIG FILES #####
+# Update Lucee Server Admin Password
 sed -i "s/\${SERVER-HSPW}/$LUCEESERVERPASS/g" /opt/lucee/server/lucee-server/context/lucee-server.xml
 sed -i "s/\${SERVER-HSPW-SALT}/$LUCEESERVERSALT/g" /opt/lucee/server/lucee-server/context/lucee-server.xml
+
+# Update Lucee Datasource Information
 sed -i "s/\${MYSQL_ROOT_PASSWORD}/${MYSQL_ROOT_PASSWORD}/g" /opt/lucee/server/lucee-server/context/lucee-server.xml
 sed -i "s/\${MYSQL_HOST}/${MYSQL_HOST}/g" /opt/lucee/server/lucee-server/context/lucee-server.xml
+sed -i "s/\${MYSQL_DATABASE}/${MYSQL_DATABASE}/g" /opt/lucee/server/lucee-server/context/lucee-server.xml
+
+# Update Lucee Web Admin Password
 sed -i "s/\${WEB-HSPW}/$LUCEEWEBPASS/g" /opt/lucee/web/lucee-web.xml.cfm
 sed -i "s/\${WEB-HSPW-SALT}/$LUCEEWEBSALT/g" /opt/lucee/web/lucee-web.xml.cfm
-# === END: COPIED FROM SLATWALL-LUCEE IMAGE ===
 
+# Setup MySQL Password
 debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD"
+
+# Install MySQL
 apt-get -y install mysql-server
 
 echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" > /mysql_init.sql

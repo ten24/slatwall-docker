5b0e8e257477c87f9db9c2a63cc0112e14020606
Sumit Verma <sumit.verma@ten24web.com>
2017-06-11 01:19:40 -0400

    finishing up slatwall quickstart fixes

--- slatwall-quickstart/entrypoint.sh
+++ slatwall-quickstart/entrypoint.sh
@@ -10,6 +10,9 @@ wget -nv https://github.com/ten24/slatwall/archive/master.zip -O /root/slatwall.
 	rm -rf /root/slatwall.zip && \
 	rm -rf /root/slatwall-master
 
+# Copy over the configFramework.cfm file so that it displays full errors
+cp /root/configFramework.cfm /var/www/custom/config/configFramework.cfm
+
 ##### UPDATE LUCEE CONFIG FILES #####
 # Update Lucee Server Admin Password
 sed -i "s/\${SERVER-HSPW}/$LUCEESERVERPASS/g" /opt/lucee/server/lucee-server/context/lucee-server.xml
@@ -24,6 +27,6 @@ sed -i "s/\${MYSQL_DATABASE}/${MYSQL_DATABASE}/g" /opt/lucee/server/lucee-server
 sed -i "s/\${WEB-HSPW}/$LUCEEWEBPASS/g" /opt/lucee/web/lucee-web.xml.cfm
 sed -i "s/\${WEB-HSPW-SALT}/$LUCEEWEBSALT/g" /opt/lucee/web/lucee-web.xml.cfm
 
-catalina.sh run
+echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" > /mysql_init.sql
 
 exec "$@"
